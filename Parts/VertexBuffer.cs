a1afa3866c965c16219346dd5226db782116939f
Tamschi <MailTamschi@googlemail.com>
2019-02-09 19:26:05 +0100

    Exposed VertexBuffer.Header and renamed chunks to attributes

--- Parts/VertexBuffer.cs
+++ Parts/VertexBuffer.cs
@@ -26,16 +26,17 @@ namespace TQ.Mesh.Parts
         readonly Span<byte> _data;
         public VertexBuffer(Span<byte> data) => _data = data;
 
-        readonly struct Header
+        public ref VertexBufferHeader Header => ref _data.View<VertexBufferHeader>(0);
+        public readonly struct VertexBufferHeader
         {
             public readonly int ChunkCount;
             public readonly int Stride;
             public readonly int VertexCount;
         }
 
-        unsafe static readonly int CHUNK_IDS_OFFSET = sizeof(Header);
+        unsafe static readonly int ATTRIBUTE_IDS_OFFSET = sizeof(VertexBufferHeader);
 
-        public enum ChunkId : int
+        public enum AttributeId : int
         {
             Position,
             Normal,
@@ -47,30 +48,30 @@ namespace TQ.Mesh.Parts
             Bytes = 14 //UNKNOWN
         }
 
-        public static int GetChunkSize(ChunkId chunkId)
+        public static int GetAttributeSize(AttributeId chunkId)
         {
             switch (chunkId)
             {
-                case ChunkId.Position:
-                case ChunkId.Normal:
-                case ChunkId.Tangent:
-                case ChunkId.Bitangent:
+                case AttributeId.Position:
+                case AttributeId.Normal:
+                case AttributeId.Tangent:
+                case AttributeId.Bitangent:
                     return 3 * sizeof(float);
-                case ChunkId.UV: return 2 * sizeof(float);
-                case ChunkId.Weights: return 4 * sizeof(float);
-                case ChunkId.Bones: return 4 * sizeof(byte);
-                case ChunkId.Bytes: return 4;
+                case AttributeId.UV: return 2 * sizeof(float);
+                case AttributeId.Weights: return 4 * sizeof(float);
+                case AttributeId.Bones: return 4 * sizeof(byte);
+                case AttributeId.Bytes: return 4;
                 default: throw new ArgumentException($"Unknown chunk id: {chunkId}", nameof(chunkId));
             }
         }
 
-        public Span<ChunkId> Chunks => MemoryMarshal.Cast<int, ChunkId>(_data.ViewRange<int>(CHUNK_IDS_OFFSET, _data.View<Header>(0).ChunkCount));
+        public Span<AttributeId> Attributes => MemoryMarshal.Cast<int, AttributeId>(_data.ViewRange<int>(ATTRIBUTE_IDS_OFFSET, Header.ChunkCount));
         public Span<byte> Buffer
         {
             get
             {
-                ref var header = ref _data.View<Header>(0);
-                return _data.Slice(CHUNK_IDS_OFFSET + sizeof(ChunkId) * header.ChunkCount, header.Stride * header.VertexCount);
+                ref var header = ref Header;
+                return _data.Slice(ATTRIBUTE_IDS_OFFSET + sizeof(AttributeId) * header.ChunkCount, header.Stride * header.VertexCount);
             }
         }
     }
